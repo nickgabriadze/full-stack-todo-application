cb883c095878b1f4e5add4fe3211100722ed9ee3
Nika Gabriadze <nickgabriadze@gmail.com>
2023-03-13 18:38:38 +0400

    Updated editing style and user can update the element inside of the given note

--- src/components/personal/components/showTodos/showTodos.tsx
+++ src/components/personal/components/showTodos/showTodos.tsx
@@ -13,7 +13,7 @@ interface Todo {
 
 interface EditTodo {
   title: string;
-  checked: number;
+  checked: boolean;
   category: string;
 }
 
@@ -21,11 +21,14 @@ export const ShowTodos = ({ forUser }: any) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [todos, setTodos] = useState<Todo[]>([]);
   const [error, setError] = useState<string>("");
-  const [edit, setEdit] = useState<{editID: number, edit: boolean}>({editID: -1, edit: false});
+  const [edit, setEdit] = useState<{ editID: number; edit: boolean }>({
+    editID: -1,
+    edit: false,
+  });
   const [editTodo, setEditTodo] = useState<EditTodo>({
-    title: '',
-    checked: 0,
-    category: ''
+    title: "",
+    checked: false,
+    category: "",
   });
 
   useEffect(() => {
@@ -65,25 +68,59 @@ export const ShowTodos = ({ forUser }: any) => {
               </div>
               <div className={showTodosStyle["title"]}>
                 <label>Title</label>
-                {edit.editID !== todo.ID ? <h1>{todo.title}</h1> : <input type="text" value={todo.title} onChange={(e) => setEditTodo({...editTodo, title: e.target.value })} />}
+                {edit.editID !== todo.ID ? (
+                  <h1>{todo.title}</h1>
+                ) : (
+                  <input
+                    type="text"
+                    value={todo.title}
+                    onChange={(e) =>
+                      setEditTodo({ ...editTodo, title: e.target.value })
+                    }
+                  />
+                )}
               </div>
 
               <div className={showTodosStyle["category"]}>
                 <label>Category</label>
-                {edit.editID !== todo.ID ? <h4>{todo.category}</h4> : <input type="text" value={todo.category} onChange={(e) => setEditTodo({...editTodo, category: e.target.value })} />}
+                {edit.editID !== todo.ID ? (
+                  <h4>{todo.category}</h4>
+                ) : (
+                  <input
+                    type="text"
+                    value={todo.category}
+                    onChange={(e) =>
+                      setEditTodo({ ...editTodo, category: e.target.value })
+                    }
+                  />
+                )}
               </div>
 
               <div className={showTodosStyle["checked"]}>
                 <label>Complete</label>
-                <div>
-                  {todo.checked === 0 ? (
-                    <h4>Not yet</h4>
-                  ) : (
-                    <div className={showTodosStyle["complete-icon"]}>
-                      <img src="/checked-icon.svg" width={48} height={48} />
-                    </div>
-                  )}
-                </div>
+                {edit.editID !== todo.ID ? (
+                  <div>
+                    {todo.checked === 0 ? (
+                      <h4>Not yet</h4>
+                    ) : (
+                      <div className={showTodosStyle["complete-icon"]}>
+                        <img src="/checked-icon.svg" width={48} height={48} />
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <div
+                    className={showTodosStyle["mark-as-complete"]}
+                    onClick={() =>
+                      setEditTodo({
+                        ...editTodo,
+                        checked: editTodo.checked ? false : true,
+                      })
+                    }
+                  >
+                    <h3>{!editTodo.checked  ? `Mark as complete` : `Undo check`}</h3>
+                  </div>
+                )}
               </div>
 
               <div className={showTodosStyle["added"]}>
@@ -91,20 +128,42 @@ export const ShowTodos = ({ forUser }: any) => {
                 <p>{getTimeDifference(new Date(todo.date))}</p>
               </div>
 
-              <div
-                className={showTodosStyle["edit"]}
-               
-              >
-                {edit.editID !== todo.ID ? <h2  onClick={(e) => setEdit({
-                  ...edit,
-                  editID: todo.ID,
-                  edit: true
-                })}>Edit</h2>: <h2 onClick={(e) => setEdit({
-                  ...edit,
-                  editID : -1,
-                  edit: false
-                })}>Update</h2>}
-                <img src="/edit-icon.svg" width={30} height={30} />
+              <div className={showTodosStyle["edit"]}>
+                {edit.editID !== todo.ID ? (
+                  <h2
+                    onClick={(e) =>
+                      {
+                      setEdit({
+                        ...edit,
+                        editID: todo.ID,
+                        edit: true,
+                      });
+
+                      setEditTodo({
+                        ...editTodo,
+                        title: todo.title,
+                        category: todo.category,
+                        checked: todo.checked === 0 ? false: true
+                      })
+                    }
+                    }
+                  >
+                    Edit
+                  </h2>
+                ) : (
+                  <h2
+                    onClick={(e) =>
+                      setEdit({
+                        ...edit,
+                        editID: -1,
+                        edit: false,
+                      })
+                    }
+                  >
+                    Update
+                  </h2>
+                )}
+                {edit.editID !== todo.ID ? <img src="/edit-icon.svg" width={30} height={30} /> : ""}
               </div>
             </div>
           );
