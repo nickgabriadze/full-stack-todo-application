6b61bafd9dbca04bcb9fd527cdd2ad87cfcefec1
Nika Gabriadze <nickgabriadze@gmail.com>
2023-03-16 08:43:25 +0400

    Filtering by category and completeness now works as intended

--- src/components/personal/components/filterTodos/filter.tsx
+++ src/components/personal/components/filterTodos/filter.tsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 import retrieveTodos from "../showTodos/api";
 import filterStyles from "./filter.module.css";
 import { Todo } from "../showTodos/showTodos";
-import { mapCategories } from "./filterers";
+import { filterByCategory, filterByCompleteness, mapCategories } from "./filterers";
 import { each, is } from "immer/dist/internal";
-
+import { getTimeDifference } from "../showTodos/timeDiff";
 
 const FilterTodos = ({ forUser }: any) => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [categories, setCategories] = useState<string[]>([]);
+  const [completedFilter, setCompletedFilter] = useState<boolean>(false);
   const [categoryFilterButton, setCategoryFilterButton] = useState({
     filterID: -1,
-    categoryToFilter: ""
+    categoryToFilter: "",
   });
   const [activeFilter, setActiveFilter] = useState({
     button1: false,
@@ -73,40 +74,117 @@ const FilterTodos = ({ forUser }: any) => {
                   button1: false,
                   button2: !activeFilter.button2,
                 });
+
+                setCategoryFilterButton({
+                  ...categoryFilterButton,
+                  filterID: -1,
+                  categoryToFilter: "",
+                });
               }}
             >
               Completeness
             </button>
           </div>
-          <div className={filterStyles["filter-options"]}>
-            <div className={filterStyles['categories']}>
-            {activeFilter.button1 &&
-              categories.map((eachCategory, index) => {
 
-                return (
-                  
+          <div className={filterStyles["filter-options"]}>
+            <div className={filterStyles["categories"]}>
+              {activeFilter.button1 &&
+                categories.map((eachCategory, index) => {
+                  return (
                     <div key={index}>
                       <button
-                      onClick={() => {
-                        setCategoryFilterButton({
-                          ...categoryFilterButton,
-                          filterID: index,
-                          categoryToFilter:eachCategory
-                        })
-                      }}
-
-                      style={index === categoryFilterButton.filterID ? { backgroundColor: "rgb(225, 220, 234)"} : { backgroundColor: "inherit"}}
-                      >{eachCategory}</button>
+                        onClick={() => {
+                          setCategoryFilterButton({
+                            ...categoryFilterButton,
+                            filterID: index,
+                            categoryToFilter: eachCategory,
+                          });
+                        }}
+                        style={
+                          index === categoryFilterButton.filterID
+                            ? { backgroundColor: "rgb(225, 220, 234)" }
+                            : { backgroundColor: "inherit" }
+                        }
+                      >
+                        {eachCategory}
+                      </button>
                     </div>
-                  
-                );
-              })
-              }
-              </div>
-              <div>
+                  );
+                })}
+            </div>
 
-              </div>
+            <div className={filterStyles["completed-filter"]}>
+              {activeFilter.button2 && (
+                <>
+                  <button 
+                  style={completedFilter ? { backgroundColor: "rgb(225, 220, 234)" }
+                  : { backgroundColor: "inherit" }}
+                  onClick={() => {setCompletedFilter(true)}}>Complete</button>
+                  <button 
+                  style={!completedFilter ? { backgroundColor: "rgb(225, 220, 234)" }
+                  : { backgroundColor: "inherit" }}
+                  onClick={() => {setCompletedFilter(false)}}>Incomplete</button>
+                </>
+              )}
+            </div>
 
+            <div className={filterStyles["filtered"]}>
+              {activeFilter.button1 && categoryFilterButton.filterID !== -1 && (
+                <div className={filterStyles["filtered-by-categories"]}>
+                  {filterByCategory(
+                    todos,
+                    categoryFilterButton.categoryToFilter
+                  ).map((eachTodo) => {
+                    return (
+                      <div
+                        key={eachTodo.ID}
+                        className={filterStyles["each-filtered-by-category"]}
+                      >
+                        <div>
+                          <p>Title</p>
+                          <h1>{eachTodo.title}</h1>
+                        </div>
+                        <div>
+                          <p>Completed</p>
+                          {eachTodo.checked === 0 ? (
+                            <h3>Not yet</h3>
+                          ) : (
+                            <img src="/checked-icon.svg"></img>
+                          )}
+                        </div>
+                        <div>
+                          <p>Added</p>
+                          <h4>{getTimeDifference(new Date(eachTodo.date))}</h4>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+
+              {activeFilter.button2 && (completedFilter === true || completedFilter === false) && (
+               <div className={filterStyles['filtered-by-completeness']}>
+                  {
+                    filterByCompleteness(todos, completedFilter).map((eachTodo, index) => {
+                      return(<div key={index} className={filterStyles['each-filtered-by-completeness']}>
+                        <div>
+                          <p>Title</p>
+                          <h1>{eachTodo.title}</h1>
+                        </div>
+                        <div>
+                          <p>Category</p>
+                         <h2>{eachTodo.category}</h2>
+                        </div>
+                        <div>
+                          <p>Added</p>
+                          <h4>{getTimeDifference(new Date(eachTodo.date))}</h4>
+                        </div>
+                      </div>)
+                  })
+                }
+               </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- src/components/personal/components/filterTodos/filterers.ts
+++ src/components/personal/components/filterTodos/filterers.ts
@@ -1,11 +1,14 @@
 import { Todo } from "../showTodos/showTodos";
 
+export const mapCategories = (todos: Todo[]) => {
+  return [...new Set(todos.map((todo) => todo.category))];
+};
 
-export const mapCategories = (todos:Todo[]) =>{ 
-  
-    return [...new Set(todos.map(todo => todo.category))];
-}
+export const filterByCategory = (todos: Todo[], category: string) => {
+  return todos.filter((todo) => todo.category === category);
+};
 
-export const filterChecked = (todos:Todo[], toggle: boolean) => {
-
-}
+export const filterByCompleteness = (todos: Todo[], completed: boolean) => {
+  const checked = completed ? 1 : 0;
+  return todos.filter((todo) => todo.checked === checked);
+};
