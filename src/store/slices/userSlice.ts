f72dc65ecab611fe48384ad54ef176b01dce8420
Nika Gabriadze <nickgabriadze@gmail.com>
2023-03-19 20:30:17 +0400

    Removed "stayLoggedIn" property from the userSlice

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -2,12 +2,10 @@ import {createSlice} from "@reduxjs/toolkit";
 
 interface UserState {
     username: string;
-    stayLoggedIn: boolean
 }
 
 const initialState:UserState = {
     username: "",
-    stayLoggedIn: false
 }
 
 export const userSlice = createSlice({
@@ -22,18 +20,11 @@ export const userSlice = createSlice({
                 username: action.payload
             }
         },
-
-        setStayLoggedIn: (state, action) => {
-            return {
-                ...state,
-                stayLoggedIn: action.payload
-            }
-        }
     }
 
 
 
 });
 
-export const {setTodoerUsername, setStayLoggedIn} = userSlice.actions;
+export const {setTodoerUsername} = userSlice.actions;
 export default userSlice.reducer;
